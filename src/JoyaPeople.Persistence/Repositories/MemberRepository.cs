3b8f31a3930e719ba15eaccf54c6f297bcc6772e
Matthew Nichols <matthew@matthew-nichols.com>
2013-06-02 08:45:33 -0600

    Basic persistence working.
    Create super simple persistence layer.
    Wrote happy path unit test.  All green.

---
+++ src/JoyaPeople.Persistence/Repositories/MemberRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JoyaPeople.Domain;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace JoyaPeople.Persistence.Repositories
+{
+    public class MemberRepository
+    {
+        /// <summary>
+        /// Creates connection to MongoDB server and returns reference 
+        /// to desired MongoCollection.
+        /// </summary>
+        private MongoCollection<Member> GetMongoCollection()
+        {
+            //Create MongoClient with hard coded connection string.  Clearly 
+            //a hard coded connection string is a bad idea in almost any real 
+            //application and we won't tolerate it here for very long.
+            var mongoClient = new MongoClient("mongodb://localhost");
+            
+            //Get a refernce to the server
+            var mongoServer = mongoClient.GetServer();
+            mongoServer.Connect();
+            
+            //Connect to the database. Same concern here about the hard coded 
+            //database name.
+            var mongoDatabase = mongoServer.GetDatabase("mongoPeople");
+
+            //Finally return a reference to the Members collection. Hard coding
+            //the collection name is probably ok, unless we will be storing to 
+            //multiple different collections.  
+            return mongoDatabase.GetCollection<Member>("members");
+        }
+
+        public void Save(Member member)
+        {
+            var mongoCollection = GetMongoCollection();
+            mongoCollection.Save(member);
+        }
+    }
+}
---
+++ src/JoyaPeople.UnitTests/Persistense/MemberRepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JoyaPeople.Domain;
+using JoyaPeople.Persistence.Repositories;
+using NUnit.Framework;
+
+namespace JoyaPeople.UnitTests.Persistense
+{
+    [TestFixture]
+    public class MemberRepositoryTests
+    {
+        [Test]
+        public void SaveTest_HappyPath()
+        {
+            var member = new Member
+                {
+                    FirstName = "John", 
+                    LastName = "Smith"
+                };
+
+            var memberRepository = new MemberRepository();
+
+            memberRepository.Save(member);
+        }
+    }
+}
